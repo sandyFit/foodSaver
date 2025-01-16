748bf3ac4f9ceb66e3b0899cd723d51c3b7fa9f1
SandyFit <sandy07r@gmail.com>
2025-01-15 23:58:14 -0500

    Copy adjustment

--- client/src/components/cards/FeaturesCard.jsx
+++ client/src/components/cards/FeaturesCard.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const FeaturesCard = () => {
     return (
         <article className="w-full flex flex-col items-center gap-4">
-            
+            <h2>
+                Controla lo que tienes
+            </h2>
+            <p>
+                Gestiona fácilmente tu inventario y mantente al tanto de las fechas de caducidad.
+            </p>
         </article>
     )
 }
--- client/src/pages/Hero.jsx
+++ client/src/pages/Hero.jsx
@@ -7,13 +7,15 @@ const Hero = () => {
                 <div className="w-full flex flex-col justify-center items-center gap-6">
                     <div className="flex flex-col">
                         <h1 className='text-yellow-100 text-[8rem] font-extrabold mt-10 uppercase 
-                            tracking-tight leading-[120px] text-center'>
+                            tracking-tight leading-[115px] text-center'>
                             Despídete del Desperdicio de Alimentos
                         </h1>
                     </div>
-                    <p className="w-2/3 text-xl text-[#fff] text-center">
-                        Food Saver es la aplicación que te permite llevar un control de los alimentos que tienes
-                        en tu hogar, reducir el desperdicio y ahorrar dinero fácilmente.
+                    <p className="w-[68%] text-xl text-[#fff] text-center">
+                        FoodSaver es la app que te permite organizar los alimentos en tu hogar,
+                        reducir el desperdicio y ahorrar dinero fácilmente. Optimiza tus compras,
+                        prolonga la vida útil de tus alimentos y contribuye al cuidado del planeta,
+                        todo desde un mismo lugar.
                     </p>
 
                     <div className="w-2/3 flex justify-center gap-5">
